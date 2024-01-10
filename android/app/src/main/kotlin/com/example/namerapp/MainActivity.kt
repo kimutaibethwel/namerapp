ca33401e447637dddded3cb208cf54116d6c7023
kimutaibethwel <155556127+kimutaibethwel@users.noreply.github.com>
2024-01-10 22:25:33 +0300

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/namerapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.namerapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/chessman/flutter_linux_3.16.5-stable/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/chessman/Documents/flutterProjects/namerapp" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.0.1+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/chessman/flutter_linux_3.16.5-stable/flutter"
+  "PROJECT_DIR=/home/chessman/Documents/flutterProjects/namerapp"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8zZjNlNTYwMjM2NTM5YjdlMjcwMmY1YWM3OTBiMmE0NjkxYjMyZDQ5Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/chessman/Documents/flutterProjects/namerapp/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/chessman/Documents/flutterProjects/namerapp/lib/main.dart"
+)
